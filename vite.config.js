c1940a0aa51dd3c0203d45b855c127b285f69e80
farhanbin65 <samibin19@gmail.com>
2026-05-23 18:36:26 +0100

    Enable HTTPS for Vite dev server

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   optimizeDeps: {
     include: ['kociemba-wasm']
+  },
+  server: {
+    https: true,
+    host: true,
   },
   worker: {
     format: 'es'
